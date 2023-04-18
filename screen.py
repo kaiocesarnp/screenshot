0609c0c0e0dfb32ad50cb34ae0c2ae9d8f18b5a9
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-18 09:13:00 -0300

    screenshot

--- screen.py
+++ screen.py
@@ -2,3 +2,9 @@ import pyautogui
 
 screenshot = pyautogui.screenshot()
 screenshot.save('screenshot.png')
+
+# Explicação do código:
+# import pyautogui = importa a biblioteca PyAutoGUI, que é uma biblioteca Python que fornece funções 
+                        # para automatizar a interação com a interface gráfica do usuário (GUI) 
+                        # em sistemas operacionais Windows, macOS e Linux.
+
