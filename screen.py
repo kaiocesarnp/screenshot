029da3317e5a0fc9a83e41c83e47ff3ecc599838
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-18 09:15:52 -0300

    screenshot

--- screen.py
+++ screen.py
@@ -4,7 +4,17 @@ screenshot = pyautogui.screenshot()
 screenshot.save('screenshot.png')
 
 # Explicação do código:
-# import pyautogui = importa a biblioteca PyAutoGUI, que é uma biblioteca Python que fornece funções 
+# import pyautogui : importa a biblioteca PyAutoGUI, que é uma biblioteca Python que fornece funções 
                         # para automatizar a interação com a interface gráfica do usuário (GUI) 
                         # em sistemas operacionais Windows, macOS e Linux.
+
+# screenshot = pyautogui.screenshot(): Essa linha de código chama a função screenshot() da biblioteca
+                                        #  PyAutoGUI para tirar uma captura de tela do monitor. 
+                                        # A imagem capturada é armazenada em uma variável chamada 
+                                        # screenshot.
+
+# screenshot.save('screenshot.png'): Essa linha de código chama o método save() do objeto screenshot 
+                                        # para salvar a captura de tela como uma imagem PNG. 
+                                        # O nome do arquivo é definido como "screenshot.png", que é 
+                                        # o nome do arquivo de saída. 
 
